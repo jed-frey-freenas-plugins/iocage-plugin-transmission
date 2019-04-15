68d1b615bd1a47d87512135452e6391486c0fbbc
Jed <jed@g5.lan>
2019-04-15 04:12:02 -0400

    .

--- post_install.sh
+++ post_install.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
-sysrc -f /etc/rc.conf transmission_enable="YES"
-sysrc -f /etc/rc.conf transmission_download_dir="/usr/local/etc/transmission/home/Downloads"
-# Start service once to create the files needed
-service transmission start
-sleep 2 # It can take a few seconds.
-service transmission stop
-sleep 2 # If it's still running, our changes won't propagate, as they're lost on restart. This is to be safe.
 
-# Without the whitelist disabled, the user will not be able to access it. They can reenable and manually whitelist their IP's.
-SETTINGS="/usr/local/etc/transmission/home/settings.json"
-echo "Disabling RPC whitelist, you may want to reenable it with the specific IP's you will access transmission with by editing $SETTINGS"
-sed -i '' 's/    "rpc-whitelist-enabled": true,/    "rpc-whitelist-enabled": false,/' $SETTINGS
-
-#Create a Download Directory
-mkdir -p /usr/local/etc/transmission/home/Downloads
-chmod 755 /usr/local/etc/transmission/home/Downloads
-
-# Start the service
-service transmission start
+/root/post_install_scripts/01_transmission.sh
+/root/post_install_scripts/04_nginx.sh
+/root/post_install_scripts/05_svscan.sh
